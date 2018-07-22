110498d005e7efc2407d25c704ebb03cdc82ab30
ryym <ryym.64@gmail.com>
2018-07-22 09:37:28 +0900

    feat(screen): Define state struct

--- src/screen.rs
+++ src/screen.rs
@@ -1,12 +1,16 @@
-use coord::Dir;
+use coord::{Coord, Dir};
 use game::Game;
-use std::io::{stdout, Read, Write};
+use std::io::{stdout, Read, Write, Bytes};
 use std::iter;
 use std::thread;
 use std::time::Duration;
 use termion as tm;
-use termion::async_stdin;
+use termion::{async_stdin, AsyncReader};
 use termion::raw::IntoRawMode;
+
+struct State {
+    field_pos: Coord,
+}
 
 pub fn play(mut game: Game) {
     let stdout = stdout();
@@ -20,6 +24,10 @@ pub fn play(mut game: Game) {
         tm::cursor::Goto(1, 1),
         tm::cursor::Hide,
     ).unwrap();
+
+    let state = State {
+        field_pos: Coord(1, 2),
+    };
 
     let interval = Duration::from_millis(32);
     let mut t = 0;
@@ -43,21 +51,13 @@ pub fn play(mut game: Game) {
                 Err(_) => {
                     render_game_over(&mut stdout);
                     stdout.flush().unwrap();
-
-                    // Wait any key inputs before exiting.
-                    loop {
-                        match stdin.next() {
-                            Some(_) => break,
-                            _ => {}
-                        }
-                        thread::sleep(interval);
-                    }
+                    wait_any_key_input(&mut stdin, interval);
                     break;
                 }
             }
         }
 
-        render(&game, 2, &mut stdout);
+        render(&mut stdout, &game, &state);
         stdout.flush().unwrap();
 
         thread::sleep(interval);
@@ -67,11 +67,14 @@ pub fn play(mut game: Game) {
     write!(stdout, "{}", tm::cursor::Show).unwrap();
 }
 
-fn render(g: &Game, y: usize, w: &mut Write) {
+fn render(w: &mut Write, g: &Game, state: &State) {
     let field = g.field();
+    let Coord(x, y) = state.field_pos;
+    let x = x as usize;
+    let y = y as usize;
 
     for (i, line) in field.lines_iter().enumerate() {
-        write!(w, "{}|", tm::cursor::Goto(1, (i + y) as u16)).unwrap();
+        write!(w, "{}|", tm::cursor::Goto(x as u16, (i + y) as u16)).unwrap();
         for cell in line.iter() {
             match cell {
                 Some(block) => write!(w, "{} ", block.chr),
@@ -81,7 +84,7 @@ fn render(g: &Game, y: usize, w: &mut Write) {
         write!(w, "|").unwrap();
     }
 
-    write!(w, "{}", tm::cursor::Goto(1, (field.height() + y) as u16)).unwrap();
+    write!(w, "{}", tm::cursor::Goto(x as u16, (field.height() + y) as u16)).unwrap();
     let width = field.width();
     for floor in iter::repeat("--").take(width + 1) {
         write!(w, "{}", floor).unwrap();
@@ -91,4 +94,14 @@ fn render(g: &Game, y: usize, w: &mut Write) {
 fn render_game_over(w: &mut Write) {
     write!(w, "{}", tm::cursor::Goto(1, 1)).unwrap();
     write!(w, "====== GAME OVER ======").unwrap();
+}
+
+fn wait_any_key_input(stdin: &mut Bytes<AsyncReader>, interval: Duration) {
+    loop {
+        match stdin.next() {
+            Some(_) => break,
+            _ => {}
+        }
+        thread::sleep(interval);
+    }
 }

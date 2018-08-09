181b6988714602d83976ef242ed90489399eab04
ryym <ryym.64@gmail.com>
2018-08-09 09:35:10 +0900

    Add normal key binding

--- src/inputs/keys.rs
+++ src/inputs/keys.rs
@@ -12,6 +12,7 @@ macro_rules! hash_map {
 }
 
 pub enum KeyConverter {
+    Normal,
     Vim,
 }
 
@@ -19,6 +20,16 @@ impl KeyConverter {
     pub fn key_to_order(&self) -> HashMap<Key, Order> {
         use super::Order::*;
         match self {
+            KeyConverter::Normal => hash_map!{
+                Key::Left => Move(Dir::Left),
+                Key::Right => Move(Dir::Right),
+                Key::Down => Move(Dir::Down),
+                Key::Char('d') => Rotate(RotateDir::AntiClockwise),
+                Key::Char('f') => Rotate(RotateDir::Clockwise),
+                Key::Char('\n') => Select,
+                Key::Char('?') => Help,
+                Key::Char('q') => Quit,
+            },
             KeyConverter::Vim => hash_map!{
                 Key::Char('h') => Move(Dir::Left),
                 Key::Char('l') => Move(Dir::Right),
